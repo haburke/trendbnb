567422b601b1ba6d5b46244a6cfed0446c79f86e
Heather Burke <59520907+haburke@users.noreply.github.com>
2024-12-03 05:12:38 +0000

    sqlalchemy test in main

--- trendbnb.py
+++ trendbnb.py
@@ -8,6 +8,25 @@ import dash_bootstrap_components as dbc
 # import local packages
 # ----------------------------------------------------------------------------------------------------------------------
 from pages.utils import dbc_css
+from .config.cred import USERNAME, PASSWORD
+
+# ======================================================================================================================
+# import non-standard library packages
+# ----------------------------------------------------------------------------------------------------------------------
+from sqlalchemy.engine import create_engine
+
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+HOST = 'subdomain.domain.tld'
+PORT = 1521
+SERVICE = 'oracle_db_service_name'
+ENGINE_PATH = f"{DIALECT}+{SQL_DRIVER}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/?service_name={SERVICE}"
+engine = create_engine(ENGINE_PATH)
+
+# == test query ========================================================================================================
+import pandas as pd
+df = pd.read_sql_query('SELECT * FROM TABLE', engine)
+print(df.head())
 
 # -- init dash ---------------------------------------------------------------------------------------------------------
 app = Dash(__name__,
