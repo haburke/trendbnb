7524bb991e5ddbec50223b592b5fb14f2ec3369c
Heather Burke <59520907+haburke@users.noreply.github.com>
2024-12-07 01:33:01 +0000

    remove login, unused data

--- trendbnb.py
+++ trendbnb.py
@@ -16,38 +16,19 @@ from pages.utils import dbc_css
 # import non-standard library packages
 # ----------------------------------------------------------------------------------------------------------------------
 
-# -- modal menus -------------------------------------------------------------------------------------------------------
-login_modal = dbc.Modal([
-    dbc.ModalBody(
-        dbc.Container([
-            dbc.FormFloating([
-                dbc.Input(type='text', id='login_username_input'),
-                dbc.Label("Username")
-            ]),
-            dbc.FormFloating([
-                dbc.Input(type='text', id='login_password_input'),
-                dbc.Label("Password")
-            ]),
-            dbc.Button("Submit", type="submit", class_name='w-100', id='login_submit_button', n_clicks=0)
-        ])
-    )], id='modal', is_open=False, centered=True, fade=True, backdrop=True, keyboard=True)
 
 # -- header ------------------------------------------------------------------------------------------------------------
 title = html.H3("Trendbnb")
 logo = html.Img(
-    src="./assets/trendbnb_logo.png", style={'height': 80, 'margin': "8px 0px 8px 15px"}
+    src="./assets/trendbnb_logo.png", style={'height': 80, 'margin': "8px 0px 8px 30px"}
 )
 
 header = dbc.Row([
     dbc.Col(logo, width="auto", align="center"), 
     dbc.Col(title, width="auto", align="center"),
-    dbc.Col([
-        dbc.Button("Login", id="login-button"),
-        login_modal
-    ])
+
     ], style={"background-color":"var(--bs-dark)", 'box-shadow': "0px 0px 5px 0px #111111"})
 
-login_button = dbc.Button("Click Me", id="logged-in-button", n_clicks=0)
 
 # -- init dash ---------------------------------------------------------------------------------------------------------
 app = Dash(__name__,
@@ -57,40 +38,9 @@ app = Dash(__name__,
 
 app.layout = html.Div([
     header,
-
-    html.Div(id="homepage-div"),
-    dcc.Store(id="logged-in"),
-    html.Div([
-        login_button,
-    ], id="login-button-div"),
-    html.Div(id="hidden-div")
+    page_container
 ])
 
-# Login button triggers modal
-@callback(
-    Output("modal", "is_open"),
-    [Input("login-button", "n_clicks"),
-     Input("login_submit_button", "n_clicks")],
-    [State("modal", "is_open")],
-    prevent_initial_call=True,
-)
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2:
-        return not is_open
-    return is_open
-
-@callback(
-    [Output("hidden-div", "children"),
-     Output("login-button-div", "children"),
-     Output("homepage-div", "children")],
-    [Input("login_submit_button", "n_clicks"),
-     Input("login_username_input", "value"),
-     Input("login_password_input", "value")]
-)
-def submit_login(n, username, password):
-    if (n and username == "admin" and password == "admin") or debug_mode:
-        return html.Div(page_container), html.H1("yourname"), html.Div()
-    return html.Div(), login_button, html.Div()
 
 if __name__ == "__main__":
     debug_mode = True   
